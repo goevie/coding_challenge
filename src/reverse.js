8fb44f63bddcf0782011cc4c1b0d45ec84ba0439
minim777 <79686154+minim777@users.noreply.github.com>
2021-08-14 17:45:43 +1000

    First working solution

--- src/reverse.js
+++ src/reverse.js
@@ -1,10 +1,33 @@
-
 const reverseSentence = (sentence, reverseWords, reverseLetters) => {
-
   // This is where the logic goes, current implementation doesn't pass all tests!
-  return sentence;
-}
 
-module.exports =
-  { reverseSentence };
+  function swapInput(array, startIdx, endIdx) {
+    // swaps elements without creating auxilary data structures
+    while (startIdx < endIdx) {
+      let tmp = array[startIdx];
+      array[startIdx] = array[endIdx];
+      array[endIdx] = tmp;
+      startIdx++;
+      endIdx--;
+    }
+    return array;
+  }
 
+  sentence = sentence.trim().split(" ");
+  if (reverseWords) {
+    // passes in array of words
+    sentence = swapInput(sentence, 0, sentence.length - 1);
+  }
+
+  if (reverseLetters) {
+    // access substrings in sentence
+    for (let i = 0; i < sentence.length; i++) {
+      // swapInput requires an array - change word into character array
+      sentence[i] = swapInput(sentence[i].split(""), 0, sentence[i].length - 1).join("");
+    }
+  }
+
+  return sentence.join(" ");
+};
+
+module.exports = { reverseSentence };
