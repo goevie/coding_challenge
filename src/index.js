75a08209f1fca75b4c157b3e847aad33d762a648
minim777 <79686154+minim777@users.noreply.github.com>
2021-08-14 19:29:27 +1000

    CLI working, small issure with command flags

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
+const reverse = require('./reverse');
 
-console.log("There is no code yet!!");
+// using package yargs
+const args = require('yargs')(process.argv.slice(2))
+let string  = args.argv._[0];
+let arg1, arg2 = false;
+
+if (args.argv._.indexOf('word') < 0) {
+    arg1 = true;
+}
+
+if (args.argv._.indexOf('letter') < 0) {
+    arg2 = true;
+}
+
+try {
+    console.log(reverse.reverseSentence(string, arg1, arg2));
+} catch(error) {
+    console.log('Invalid format of arguments, refer to docs');
+}
